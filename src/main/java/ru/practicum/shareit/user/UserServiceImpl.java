a15866ea8697f887d89d52d0813eac1470d7c43d
verazhadovskaya@yandex.ru <vera.zhadovskaya@ax-team.corp>
2023-11-21 13:35:14 +0300

    checkstyle

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
